82c1d20002210f7533f4350245ac87e864a0da1d
Cihan Soylu <cihansoylu@gmail.com>
2018-06-22 20:38:53 -0400

    First commmit

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -8,7 +8,7 @@ square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','45
 unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+unitlist = unitlist + [[''.join(x) for x in zip(rows, cols)]] + [[''.join(x) for x in zip(rows[::-1], cols)]]
 
 
 # Must be called after all units (including diagonals) are added to the unitlist
@@ -49,8 +49,43 @@ def naked_twins(values):
     strategy repeatedly).
     """
     # TODO: Implement this function!
-    raise NotImplementedError
-
+    
+    #List of boxes with exactly two possible values
+    boxes_of_length_two = []
+    
+    #List of tuples (box_1, box_2) where box_1 and box_2 have the same two possible values and they are peers of each other
+    twins = []
+    
+    #Determine all boxes with exactly two possible values
+    for box in boxes:
+        if len(values[box]) == 2:
+            boxes_of_length_two.append(box)
+    
+    #Find the twins
+    if len(boxes_of_length_two) == 0:
+        return values
+    
+    for box in boxes_of_length_two:
+        #First find the boxes of length two that are peers of the box. 
+        peers_of_length_two = list(set(peers[box]).intersection(set(boxes_of_length_two)))
+        if len(peers_of_length_two) > 0:
+            # If there are peers of length two, check if they are actual twins.
+            for peer in peers_of_length_two:
+                if values[peer] == values[box]:
+                    twins.append((box, peer))
+    
+    #For each twin remove the twin_values from the other boxes in the units that contains the twins. 
+    for twin in twins:
+        for unit in units[twin[0]]:
+            # If the unit contains both twins
+            if twin[1] in unit: 
+                for box in unit:
+                    if box not in twin: #Don't remove the values from the twins.
+                        for v in values[twin[0]]:
+                            values[box] = values[box].replace(v, '')   
+            
+    return values
+    
 
 def eliminate(values):
     """Apply the eliminate strategy to a Sudoku puzzle
@@ -69,7 +104,19 @@ def eliminate(values):
         The values dictionary with the assigned values eliminated from peers
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+    
+    #for box, value in values.items():
+    #    if len(value) == 1:
+    #        for peer in peers[box]: 
+    #            values[peer] = values[peer].replace(value, '')           
+    #return values
 
 
 def only_choice(values):
@@ -93,7 +140,12 @@ def only_choice(values):
     You should be able to complete this function by copying your code from the classroom
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in cols:
+            boxes_with_digit = [box for box in unit if digit in values[box]]
+            if len(boxes_with_digit) == 1:
+                values[boxes_with_digit[0]] = digit
+    return values
 
 
 def reduce_puzzle(values):
@@ -111,7 +163,24 @@ def reduce_puzzle(values):
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
     # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -134,7 +203,33 @@ def search(values):
     and extending it to call the naked twins strategy.
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    values = naked_twins(values)
+
+    values = reduce_puzzle(values)
+
+    
+    #reduce_puzzle will return false if there are no solution to the sudoku board with the current values
+    #which indicates that one or more of the current values are wrong. 
+    if values is False:
+        return False 
+    
+    #Check if the puzzle is done
+    done = True
+    for box in boxes:
+        if len(values[box]) > 1:
+            done = False
+    if done:
+        return values
+    
+    #If the puzzle is not done start the search
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 
 def solve(grid):
